d9880a05ac5d247ca711c6cd3000f4aca87fc893
aryantayal <atayal99@gmail.com>
2023-11-06 11:04:31 -0600

    mvp 1 done baby

--- components/sections/index/about.jsx
+++ components/sections/index/about.jsx
@@ -1,73 +1,72 @@
 // Core packages
-import Image from 'next/image'
+import Image from "next/image";
 
 // Section structure
-import Section from '../../structure/section';
-import Container from '../../structure/container';
+import Section from "../../structure/section";
+import Container from "../../structure/container";
 
 // Section general blocks
-import SectionTitle from '../../blocks/section.title.block'
-import SectionGridBg from '../../blocks/section.grid.block'
+import SectionTitle from "../../blocks/section.title.block";
+import SectionGridBg from "../../blocks/section.grid.block";
 
 // Section specific blocks
-import BadgesBlock from '../../blocks/about.badges.block'
-import CopyBlock from '../../blocks/about.copy.block'
+import BadgesBlock from "../../blocks/about.badges.block";
+import CopyBlock from "../../blocks/about.copy.block";
 
 // Section scss
-import about from '../../../styles/sections/index/about.module.scss';
+import about from "../../../styles/sections/index/about.module.scss";
 
 /**
  * Section: About
  * An overview of yourself.
  * Highlight your top level attributes and disciplines.
- * 
+ *
  * @returns {jsx} <About />
  */
 export default function About() {
-	return (
-		<Section classProp={about.section}>	
-			<Container spacing={['verticalXXXLrg']}>
-				<SectionTitle
-					title="About Me"
-					preTitle="Synopsis"
-					subTitle="With a diverse skill set that includes Full stack development, UI design, branding, and Photography/Cinamatography. I am a well-rounded digital professional."
-				/>
-				<section className={about.content}>
-					<div className={about.image}>
-						<img src="/img/profile-picture.jpg" alt="Aryan Tayal"/>
-						{/* <Image src="/img/family-photo.jpg" width={600} height={800}/> */}
-					</div>
-					<div className={about.copy} >
-						<CopyBlock 
-							title="Softskills that pay the bills"
-							containerClass={about.container}
-							iconClass={about.icon}
-							icon={[ 'fat', 'ear-listen' ]}
-							copy="In addition to my technical expertise — I also have strong leadership, time management, and multitasking skills—honed through my experience as a business owner and constant student. Outside of work, I enjoy trying new coffee shops with friends, shooting videos, playing golf as well managing my business and reading books. I am confident in my ability to bring passion and value to any project."
-						/>
-						<BadgesBlock 
-							title="Reasearch and planning" 
-							containerClass={about.container}
-							list={methods} 
-							fullContainer="fullContainer"
-							block="methods" 
-							icon="fingerprint"
-							copy="One of my favorite aspects of creating is seeing a project come to fruition and watching my dedication drive me to working on a vision that I believe in."
-							//invertedColor="invertedColor"
-							headerIcon={`${about.icon}`}
-						/>
-					</div>
-				</section>	
-			</Container>
-		</Section>
-	)
+  return (
+    <Section classProp={about.section}>
+      <Container spacing={["verticalXXXLrg"]}>
+        <SectionTitle
+          title="About Me"
+          preTitle="Synopsis"
+          subTitle="With a diverse skill set that includes Full stack development, UI design, branding, and Photography/Cinamatography. I am a well-rounded digital professional."
+        />
+        <section className={about.content}>
+          <div className={about.image}>
+            <img src="/img/profile-picture.jpg" alt="Aryan Tayal" />
+          </div>
+          <div className={about.copy}>
+            <CopyBlock
+              title="Softskills that pay the bills"
+              containerClass={about.container}
+              iconClass={about.icon}
+              icon={["fat", "ear-listen"]}
+              copy="In addition to my technical expertise — I also have strong leadership, time management, and multitasking skills—honed through my experience as a business owner and constant student. Outside of work, I enjoy trying new coffee shops with friends, shooting videos, playing golf as well managing my business and reading books. I am confident in my ability to bring passion and value to any project."
+            />
+            <BadgesBlock
+              title="Reasearch and planning"
+              containerClass={about.container}
+              list={methods}
+              fullContainer="fullContainer"
+              block="methods"
+              icon="fingerprint"
+              copy="One of my favorite aspects of creating is seeing a project come to fruition and watching my dedication drive me to working on a vision that I believe in."
+              //invertedColor="invertedColor"
+              headerIcon={`${about.icon}`}
+            />
+          </div>
+        </section>
+      </Container>
+    </Section>
+  );
 }
 
-const methods 	= [
-	{ key: 'planet-moon', 		name: 'User Research', 		type: 'fad' },
-	{ key: 'qrcode', 			name: 'Digital Strategy', 	type: 'fad' },
-	{ key: 'window', 			name: 'Design Systems', 	type: 'fad' },
-	{ key: 'cubes', 			name: 'Product Strategy', 	type: 'far' },
-	{ key: 'layer-plus', 		name: 'Brand Strategy', 	type: 'fad' },
-	{ key: 'solar-system', 		name: 'Operations', 		type: 'fad' },
-]
+const methods = [
+  { key: "planet-moon", name: "User Research", type: "fad" },
+  { key: "qrcode", name: "Digital Strategy", type: "fad" },
+  { key: "window", name: "Design Systems", type: "fad" },
+  { key: "cubes", name: "Product Strategy", type: "far" },
+  { key: "layer-plus", name: "Brand Strategy", type: "fad" },
+  { key: "solar-system", name: "Operations", type: "fad" },
+];
